3a31c4a46d2225715db63de075dd2b90561d568a
1h3gamer <Idaune.nathan@gmail.com>
2025-11-29 17:56:06 +0000

    circle

---
+++ Main17.py
@@ -0,0 +1,12 @@
+class circle:
+    def __init__(self, r):
+        self.radius = r
+    def area(self):
+        return self.radius**2*3.14
+    def perimeter(self):
+        return 2*self.radius*3.14
+    
+radius = int(input("Enter the radius: "))
+newcircle = circle(radius)
+print(newcircle.area())
+print(newcircle.perimeter())
